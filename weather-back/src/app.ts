c768087f2d65dd0e101607a506c57c7a4a53f70e
lucasavs <lucasavs89@gmail.com>
2025-07-01 20:47:14 +0200

    reducing time to reconnect to WS

--- weather-back/src/app.ts
+++ weather-back/src/app.ts
@@ -8,7 +8,7 @@ import cors from 'cors';
 dotenv.config();
 
 // Starting connection with the WS client
-const reconnectInterval = 1000 * 60;
+const reconnectInterval = 1000 * 5;
 const wsConnection = () => {
   const wsClient = new ws(`ws://${process.env.WEATHERSTREAMHOST}:8765`)
   // const wsClient = new ws('ws://localhost:8765');
@@ -19,7 +19,6 @@ const wsConnection = () => {
     console.log('socket error');
   });
   wsClient.on('message', (data) => {
-    // console.log(data.toString())
     const bufferedData = data.toString();
     const parsedDAta = JSON.parse(bufferedData) as cityTemperatureData;
     candlestickService.addCityData(parsedDAta)
